6673f1536ac75f9aac44064a09f6cccdadb35d63
koovisa <chad@koovisa.com>
2012-03-27 20:50:47 +0100

    wahoo! no aiming at spaces that won't fit ships. detect adjacent ships end to end and in a T.

--- contestants/wmrug/ruby_nuby_player.rb
+++ contestants/wmrug/ruby_nuby_player.rb
@@ -1,9 +1,12 @@
 class Ship
   attr_accessor :known_direction, :coords
 
-  def initialize(start_coord)
-    @coords = []
-    @coords << start_coord
+  def initialize(start_coords)
+    if (start_coords.flatten.count==2)
+      @coords = [start_coords.flatten]
+    else
+      @coords = start_coords
+    end
     @known_direction = nil
   end
 
@@ -16,10 +19,10 @@ class Ship
       return upper_pin if upper_pin and result(upper_pin, state) == :unknown
     else
       #rotate
-      return left_pin() if left_pin() and result(left_pin(), state) == :unknown
-      return right_pin() if right_pin() and result(right_pin(), state) == :unknown
-      return upper_pin() if upper_pin() and result(upper_pin(), state) == :unknown
-      return lower_pin() if lower_pin() and  result(lower_pin(), state) == :unknown
+      return left_pin if left_pin and result(left_pin, state) == :unknown
+      return right_pin if right_pin and result(right_pin, state) == :unknown
+      return upper_pin if upper_pin and result(upper_pin, state) == :unknown
+      return lower_pin if lower_pin and  result(lower_pin, state) == :unknown
       
     end
 
@@ -77,15 +80,34 @@ class RubyNubyPlayer
   def take_turn(state, ships_remaining)  
     @state = state
     if @current_move
-		  last_result = state[@current_move[1]][@current_move[0]]
+      last_result = state[@current_move[1]][@current_move[0]]
     end
     
-    if @previous_ships_remaining and ships_remaining.length < @previous_ships_remaining.length
-        @current_ship = nil
+    if @previous_ships_remaining and ships_remaining.length < @previous_ships_remaining.length      
+        @current_ship.coords << @current_move
+        a = @previous_ships_remaining
+        @length_of_sunken_ship = 
+          a[a.map{|v| a.count(v) != ships_remaining.count(v)}.index(true)]
+
+        if @length_of_sunken_ship != @current_ship.coords.count
+          if @current_move <=> @previous_move = 1
+            @known_ships <<
+              Ship.new(@current_ship.coords.sort.last(@current_ship.coords.count - @length_of_sunken_ship))
+          else
+            @known_ships << 
+              Ship.new(@current_ship.coords.sort.first(@current_ship.coords.count - @length_of_sunken_ship))
+          end
+          #raise "sunk a ship of length #{@length_of_sunken_ship}. New ship at #{@current_ship.coords.inspect}"
+          @hunt_mode = :destroy
+        else
+          @current_ship = nil
+          @hunt_mode = :seek
+        end
+        
         last_result = :sunk
-        @hunt_mode = :seek
     end
 
+    @previous_move = @current_move
 
     if last_result == :hit
       if @current_ship
@@ -97,9 +119,8 @@ class RubyNubyPlayer
         end
       else
         @current_ship = Ship.new(@current_move)       
+        @hunt_mode = :destroy
       end
-
-      @hunt_mode = :destroy
     end
 
     if last_result == :sunk and @known_ships.length>0
@@ -109,12 +130,13 @@ class RubyNubyPlayer
 
 
     if @hunt_mode == :seek
-      @current_move = seek()
+      @current_move = seek(ships_remaining)
     else 
       adjacent = @current_ship.find_adjacent(@state)
       if adjacent
         @current_move = adjacent
       else
+        #raise "changing direction: #{@current_ship.coords.inspect}"
         if @current_ship.known_direction == :horizontal
           new_direction = :vertical
         else
@@ -136,11 +158,40 @@ class RubyNubyPlayer
     @previous_ships_remaining = ships_remaining
     @remaining_positions.delete(@current_move)
 
+    raise "Ship: #{@current_ship.inspect} Last Move: #{@previous_move.inspect}" unless @current_move
     return @current_move
   end
   
-  def seek()
-    return @remaining_positions[rand(@remaining_positions.length-1)]
+  def seek(ships_remaining)
+    coord = nil
+    while coord == nil
+      coord = @remaining_positions[rand(@remaining_positions.length-1)]
+      raise "remaining: #{@remaining_positions.inspect}" unless coord
+      if not can_fit_ship(coord, ships_remaining.min)
+        @remaining_positions.delete(coord)
+        coord = nil
+      end
+    end
+    return coord
   end
 
+  def can_fit_ship(coord, length)
+
+    pin_index = (0..coord.first-1).map{|i| @state[coord.last][i] != :unknown }.rindex(true)
+    spaces_left = pin_index ? coord.first - pin_index - 1 : coord.first
+
+    spaces_right = (coord.first+1..9).map{|i| @state[coord.last][i] != :unknown }.index(true) || 9-coord.first
+
+    pin_index = (0..coord.last-1).map{|i| @state[i][coord.first] != :unknown }.rindex(true)
+    spaces_above = pin_index ? coord.last - pin_index -1 : coord.last
+
+    spaces_below = (coord.last+1..9).map{|i| @state[i][coord.first] != :unknown }.index(true) || 9-coord.last
+
+
+    if spaces_above + spaces_below + 1 < length and spaces_left + spaces_right + 1 < length
+      return false
+    end
+
+    return true
+  end
 end
